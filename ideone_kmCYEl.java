9af259b4fc32d0a03a37cd348f332dbb0d8dff95
anvita-anand <anvitaanand1311@gmail.com>
2017-07-09 14:01:20 +0530

    Add files via upload

---
+++ ideone_kmCYEl.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+ public static void main(String[ ] arg)
+	{
+		String S1= "Hello";
+		int i=0;
+		for (i=0; i<5; i++)
+		System.out.println("Hello");
+	}
+}
