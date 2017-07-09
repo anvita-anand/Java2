35038deb31a045df3075caf96701b87c9170006b
anvita-anand <anvitaanand1311@gmail.com>
2017-07-09 13:55:26 +0530

    Add files via upload

---
+++ ideone_LiOVJK.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+ public static void main(String[ ] arg)
+	{
+		System.out.println("Enter a year:");
+		int Year=2017;
+		if (Year%4==0)
+		System.out.println(Year+ " is a leap year");
+		else
+		System.out.println(Year+ " is not a leap year");
+	}
+}
