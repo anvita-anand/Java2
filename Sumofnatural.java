442cf90e3aa02f5a6ad5022c66c55e8f846d4139
anvita-anand <anvitaanand1311@gmail.com>
2017-07-09 14:31:04 +0530

    Add files via upload

---
+++ Sumofnatural.java
@@ -0,0 +1,14 @@
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+ public static void main(String[ ] arg)
+	{
+		int n=7;
+		int Sum;
+		Sum= (n*(n+1))/2;
+		System.out.println("The sum of " +n+ " natural numbers is " +Sum);
+	}
+}
