9c8a6de877386be02d8a3110b3c7fcc5f4bf0355
anvita-anand <anvitaanand1311@gmail.com>
2017-07-09 12:55:03 +0530

    Add files via upload

---
+++ ideone_YN6V8H.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number=0;
+		if (number>0)
+		System.out.println("Number is POSITIVE");
+		else if (number==0)
+		System.out.println("Number is ZERO");
+		else if (number<0)
+		System.out.println("Number is NEGATIVE");
+	}
+}
