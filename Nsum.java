3ad4593b804439fa6d8267f90c26dfce08c77cd2
anvita-anand <anvitaanand1311@gmail.com>
2017-07-09 14:54:45 +0530

    Add files via upload

---
+++ Nsum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N=100;
+		int i;
+		int Sum=0;
+		for (i=0; i<N+1; i++)
+		{
+			Sum=Sum+i;
+			System.out.println("The sum is " +Sum);
+		}
+	}
+}
