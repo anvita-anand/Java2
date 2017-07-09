4580877bd8ec0754506abe812ce4bc395a186d02
anvita-anand <anvitaanand1311@gmail.com>
2017-07-09 13:01:16 +0530

    Add files via upload

---
+++ ideone_E4syAo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number=63;
+		if (number%2==0)
+		System.out.println("Number is Even");
+		else
+		System.out.println("Number is Odd");
+	}
+}
